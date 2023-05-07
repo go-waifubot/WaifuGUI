128163523352b58a2424a3268102026bf0d8fa0e
Karitham <kar@karitham.dev>
2023-05-07 17:28:54 +0000

    fix w sort

--- src/components/list/FilterBar.tsx
+++ src/components/list/FilterBar.tsx
@@ -6,7 +6,7 @@ export default () => {
   return (
     <div class="flex rounded-xl flex-row flex-wrap md:flex-nowrap gap-8 justify-between h-full">
       <CharFilter class="w-full" />
-      <CharSort class="w-full full:w-48" />
+      <CharSort class="w-full md:w-48" />
       <ShowAllButton class="w-full md:w-48" />
     </div>
   );
